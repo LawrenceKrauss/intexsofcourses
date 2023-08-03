78a4b11cbc15d237a11bb7d8eb28fa438fe72f8a
Denis Damshel <denisdamshel@gmail.com>
2023-08-03 15:07:19 +0300

    added a print method

--- Main.java
+++ Main.java
@@ -56,7 +56,7 @@ class Main {
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                System.out.printf("%0," + (size * size + "").length() + "d ", snakeMatrix[i][j]);
+                System.out.printf("%0," + (size * size + "").length() + "d,", snakeMatrix[i][j]);
             }
             System.out.printf("%0," + (size * size + "").length() + "d\n", snakeMatrix[i][size - 1]);
         }
@@ -75,6 +75,15 @@ class Main {
         } else if(direction[0] == -1 && direction[1] == 0){
             direction[0] = 0;
             direction[1] = 1;
+        }
+    }
+
+    private static void printMatrix(int[][] matrix){
+        for (int rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < matrix[rowIndex].length - 1; columnIndex++) {
+                System.out.printf("%0,2d,", matrix[rowIndex][columnIndex]);
+            }
+            System.out.printf("%0,2d\n", matrix[rowIndex][matrix[rowIndex].length - 1]);
         }
     }
 }
